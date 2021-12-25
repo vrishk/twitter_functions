63344e06431e61c4c41d2ebc209d8e02cc264d31
vrishk <krishnavrishab@gmail.com>
2021-12-24 21:32:34 -0800

    chore: test other funcs

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from twitter_funcs import TwitterLoader
 
 
 def main():
     t = TwitterLoader("outputs/test")
-    t.all_tweets(["jack", "elonmusk"])
+    t.tweets(["elonmusk", "jack"], since="2021-12-23")
+    t.retweets(["elonmusk", "jack"], since="2021-12-23")
 
 if __name__ == "__main__":
     main()
